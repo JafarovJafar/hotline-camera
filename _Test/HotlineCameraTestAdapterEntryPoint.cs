d16b4fb3c8d3e0b5f0ada4275b12000a55cf4742
Jafarov Jafar <48131258+JafarovJafar@users.noreply.github.com>
2025-04-09 22:30:47 +0700

    some improvements
    * add logo
    * fix mini bug
    * add test package
    * improve readme
    * increase version
    * add gif

---
+++ _Test/HotlineCameraTestAdapterEntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Shafir.HotlineCameraSpace.Test
+{
+    internal class HotlineCameraTestAdapterEntryPoint : MonoBehaviour
+    {
+        [SerializeField] private Transform playerDummy;
+        [SerializeField] private HotlineCameraTestAdapter adapter;
+        [SerializeField] private HotlineCamera hotlineCamera;
+        [SerializeField] private float cameraRange = 1f;
+
+        private void Start()
+        {
+            hotlineCamera.Initialize();
+            hotlineCamera.SetTarget(playerDummy);
+            adapter.Initialize(hotlineCamera);
+        }
+
+        private void Update()
+        {
+            hotlineCamera.SetRange(cameraRange);
+        }
+    }
+}
